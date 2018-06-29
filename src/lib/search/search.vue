42cca0dd2dcebdd265eed5a3e5f6e9beb6285500
zhanghaizhi <zhanghaizhi@zhuojianchina.com>
2018-06-29 18:29:35 +0800

    zhanghaizhi

---
+++ src/lib/search/search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>这是一个搜索的组件</p>
+    </div>
+</template>
+<style lang='scss' scoped>
+
+</style>
+<script>
+    export default {
+        data(){
+           return {
+
+           }
+        },
+        components:{
+
+        },
+        methods:{
+
+        },
+        mounted(){
+
+        },
+        created(){
+
+        }
+    }
+</script>
